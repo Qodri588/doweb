c79ea54699c8605b1cc456e48e4101b90e216ee7
Mohammad Qodri <Mqodri588@gmail.com>
2024-11-10 17:14:38 +0700

    minify

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    plugins: {
+      autoprefixer: {},
+      cssnano: {
+        preset: "default",
+      },
+      "@fullhuman/postcss-purgecss": {
+        mode: "all",
+        content: ["./hugo_stats.json"],
+        dynamicAttributes: ["aria-current", "aria-hidden", "role", "type"],
+        safelist: {
+          standard: [
+            "show",
+            "showing",
+            "hide",
+            "fade",
+            /-backdrop$/,
+            /^is-/,
+            /^splide_/, // if your need carousel
+          ],
+        },
+        defaultExtractor: (content) => {
+          let els = JSON.parse(content).htmlElements;
+          els = els.tags.concat(els.classes);
+          return els;
+        },
+      },
+    },
+  };
